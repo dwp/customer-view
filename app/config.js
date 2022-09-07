4eecc7d11810f68b9e2d53670212d3861e34b137
irina-cristache <103247457+irinacristache-digital@users.noreply.github.com>
2022-09-07 09:08:37 +0100

    content changes on index page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'SCE Customer Experiments ',
+  serviceName: 'Online self-service',
 
   // Default port that prototype runs on
   port: '3000',
